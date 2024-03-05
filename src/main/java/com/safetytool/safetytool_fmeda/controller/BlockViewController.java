d9eed7eb922fa396f1b94e702c1ef3e102333683
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-03-05 11:47:24 +0530

    Added Export to Excel Functionality

--- src/main/java/com/safetytool/safetytool_fmeda/controller/BlockViewController.java
+++ src/main/java/com/safetytool/safetytool_fmeda/controller/BlockViewController.java
@@ -6,7 +6,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import com.safetytool.safetytool_fmeda.model.BlockDataModel;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -14,7 +24,8 @@ public class BlockViewController {
 
     @FXML
     private TableView<BlockDataModel> tableView;
-
+    @FXML
+    private AnchorPane root;
     private final ObservableList<BlockDataModel> data = FXCollections.observableArrayList();
 
     public void setMainApp() {
@@ -76,7 +87,43 @@ public class BlockViewController {
     }
 
     @FXML
-    private void exportToExcel(){
-        System.out.println("Exporting to Excel");
+    private void exportToExcel() {
+        System.out.println("Export to Excel event");
+        try (Workbook workbook = new XSSFWorkbook()) {
+            Sheet sheet = workbook.createSheet("BlockData");
+
+            // Create header row
+            Row headerRow = sheet.createRow(0);
+            for (int i = 0; i < tableView.getColumns().size(); i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(tableView.getColumns().get(i).getText());
+            }
+
+            // Populate data rows
+            for (int rowIndex = 0; rowIndex < tableView.getItems().size(); rowIndex++) {
+                Row dataRow = sheet.createRow(rowIndex + 1);
+
+                for (int colIndex = 0; colIndex < tableView.getColumns().size(); colIndex++) {
+                    Cell cell = dataRow.createCell(colIndex);
+                    cell.setCellValue(String.valueOf(tableView.getColumns().get(colIndex).getCellData(rowIndex)));
+                }
+            }
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save Excel File");
+            fileChooser.setInitialFileName("block_data.xlsx");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Excel Files", "*.xlsx"));
+
+            File file = fileChooser.showSaveDialog(root.getScene().getWindow());
+
+            // Save the workbook to a file
+            try (FileOutputStream fileOut = new FileOutputStream(file)) {
+                workbook.write(fileOut);
+            }
+
+            System.out.println("Export to Excel successful!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error exporting to Excel: " + e.getMessage());
+        }
     }
 }
